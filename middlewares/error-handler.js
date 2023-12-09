bc887e26d456befe9fb3becd440cecbd05227647
amir hossein <amirfarvahar709@gmail.com>
2023-12-09 17:56:13 +0330

    update-error-handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,6 +1,19 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500 
-  const message = err.message || "Internal server error"
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal server error";
+
+  if (err.name == "ValidationError") {
+    statusCode = 400;
+    message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+
+  if (err.name == "CastError") {
+    statusCode = 404;
+    message = `No item found with id : ${err.value}`;
+  }
+
   res.status(statusCode).json({ status: statusCode, message: message });
 };
 module.exports = errorHandler;
