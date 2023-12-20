007095e7e137c3c5971fffe71dee639c918b9561
amir hossein <amirfarvahar709@gmail.com>
2023-12-20 15:29:48 +0330

    add-async-wrapper

--- controllers/products.js
+++ controllers/products.js
@@ -1,103 +1,81 @@
 const Products = require("../products");
 const NotFoundError = require("../errors/not-found-error");
+const asyncWrapper = require("../utils/async-wrapper");
 
+const getAllTasks = asyncWrapper(async (req, res, next) => {
+  const { featured, brand, name, sort, fields, limit, page } = req.query;
+  const queryObject = {};
 
-const getAllTasks = async (req, res, next) => {
-  try {
-    const { featured, brand, name, sort, fields, limit, page } = req.query;
-    const queryObject = {};
+  // search
+  if (featured) queryObject.featured = featured === "true" ? true : false;
+  if (brand) queryObject.brand = brand;
+  if (name) queryObject.name = { $regex: name, $options: "i" };
 
-    // search
-    if (featured) queryObject.featured = featured === "true" ? true : false;
-    if (brand) queryObject.brand = brand;
-    if (name) queryObject.name = { $regex: name, $options: "i" };
-
-    // sorting
-    let result = Products.find(queryObject);
-    if (sort) {
-      const sortList = sort.split(",").join(" ");
-      result = result.sort(sortList);
-    } else {
-      result = result.sort("createdAt");
-    }
-
-    // fields
-    if (fields) {
-      const fieldsList = fields.split(",").join(" ");
-      result = result.select(fieldsList);
-    }
-
-    // pagination
-    const limitNumber = parseInt(limit) || 10;
-    const pageNumber = parseInt(page) || 1;
-    const skip = (pageNumber - 1) * limit;
-
-    const products = await result.skip(skip).limit(limitNumber);
-    res.json(products);
-  } catch (error) {
-    next(error);
+  // sorting
+  let result = Products.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
   }
-};
 
-const getSingleTask = async (req, res, next) => {
-  try {
-    const productId = req.params.id;
-    const product = await Products.findById(productId);
-    if (!product) {
-      throw new NotFoundError(`no product found with ${productId}`);
-    }
-    res.json(product);
-  } catch (error) {
-    next(error);
+  // fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
-};
 
-const createTask = async (req, res, next) => {
-  try {
-    await Products.create(req.body);
-    res.status(201).json({ status: 201, message: "product created" });
-  } catch (error) {
-    next(error);
+  // pagination
+  const limitNumber = parseInt(limit) || 10;
+  const pageNumber = parseInt(page) || 1;
+  const skip = (pageNumber - 1) * limit;
+
+  const products = await result.skip(skip).limit(limitNumber);
+  res.json(products);
+});
+
+const getSingleTask = asyncWrapper(async (req, res, next) => {
+  const productId = req.params.id;
+  const product = await Products.findById(productId);
+  if (!product) {
+    throw new NotFoundError(`no product found with ${productId}`);
   }
-};
+  res.json(product);
+});
 
-const updateTask = async (req, res, next) => {
-  try {
-    const productId = req.params.id;
-    const newProduct = await Products.findByIdAndUpdate(productId, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!newProduct) {
-      throw new NotFoundError(`no product found with ${productId}`);
-    }
+const createTask = asyncWrapper(async (req, res, next) => {
+  await Products.create(req.body);
+  res.status(201).json({ status: 201, message: "product created" });
+});
 
-    res.json(newProduct);
-  } catch (error) {
-    next(error);
+const updateTask = asyncWrapper(async (req, res, next) => {
+  const productId = req.params.id;
+  const newProduct = await Products.findByIdAndUpdate(productId, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!newProduct) {
+    throw new NotFoundError(`no product found with ${productId}`);
   }
-};
 
-const deleteTask = async (req, res, next) => {
-  try {
-    const productId = req.params.id;
-    const product = await Products.findByIdAndDelete(productId);
+  res.json(newProduct);
+});
 
-    if (!product) {
-      throw new NotFoundError(`no product found with ${productId}`);
-    }
-    res.json(product);
-  } catch (error) {
-    next(error);
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const productId = req.params.id;
+  const product = await Products.findByIdAndDelete(productId);
+
+  if (!product) {
+    throw new NotFoundError(`no product found with ${productId}`);
   }
-};
-
+  res.json(product);
+});
 
 module.exports = {
-    getAllTasks , 
-    getSingleTask , 
-    createTask , 
-    updateTask , 
-    deleteTask
-
-}
+  getAllTasks,
+  getSingleTask,
+  createTask,
+  updateTask,
+  deleteTask,
+};
